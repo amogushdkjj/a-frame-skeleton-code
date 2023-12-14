5548dbfd0edb2d51c6689ae50f1e91949153c024
amogushdkjj <142924842+amogushdkjj@users.noreply.github.com>
2023-12-14 17:42:26 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const turkey = document.getElementById("a")
 
+let xPos = 0;
+
+function moveturkey(){
+    if (xPos < 5){
+        xPos += 1
+    }
+    else {
+        xPos = 0
+    }
+    turkey.setAttribute('position', `${xPos} 0 -5`)
+    requestAnimationFrame(moveturkey)
+}
+
+moveturkey()
